7db46352a6ad3866bbf5d9d273649e3bb2afe353
Milica Jovanovic <j.milica95@yahoo.com>
2016-10-22 22:33:24 +0200

    Intial commit

---
+++ src/main/java/com/milica/controller/MyFirstController.java
@@ -0,0 +1,17 @@
+package com.milica.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MyFirstController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("poruka", "Moja prva Java Spring aplikacija!");
+        return "pozdrav";
+    }
+}
